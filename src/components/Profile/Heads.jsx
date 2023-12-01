945bc8dcc57780f81dcf216e5079975236ceff1e
T4N2 <49140760+T4N2@users.noreply.github.com>
2023-12-01 10:54:06 +0700

    Perubahan pada heads.jsx

--- src/components/Profile/Heads.jsx
+++ src/components/Profile/Heads.jsx
@@ -1,47 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { useState,useEffect } from "react";
-import { useAuth } from "../../auth";
-import { useNavigate } from 'react-router-dom';
-import { auth } from "../../config/firebase";
 
 function Heads() {
-    const [error, setError] = useState("");
-    const { currentUser } = useAuth();
-    const navigate = useNavigate(); // Ganti useHistory dengan useNavigate
-  
-    useEffect(() => {
-      if (!currentUser) {
-        // Redirect or handle the case when the user is not logged in
-        navigate('/login');
-      }
-    }, [currentUser, navigate]);
-  
-    const handleLogout = async () => {
-      try {
-        console.log('Logging out...');
-        await auth.signOut();
-        console.log('Logout successful');
-        navigate('/login');
-      } catch (error) {
-        console.error('Error during sign-out:', error.message);
-      }
-    };
-    if (!currentUser) {
-      // You can also handle the case before useEffect runs
-      return null;
-    }
-    return <div className="hed">
+    return (
+    <div className="hed">
         <div className="main">
         <a href=".././" ><img className="back" src="./images/back.png"></img></a>
-        <h4 className="title">PROFILE</h4>
+        <h4 className="title pt-6">PROFILE</h4>
         </div>
         <div className="second">
             <img className="edit" src="./images/edits.png"></img>
         </div>
-        <button onClick={handleLogout}>Sign out</button>
     </div>
-
+    )
 }
 
 export default Heads;
--- src/components/Profile/Konten.jsx
+++ src/components/Profile/Konten.jsx
@@ -1,7 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useState,useEffect } from "react";
+import { useAuth } from "../../auth";
+import { useNavigate } from 'react-router-dom';
+import { auth } from "../../config/firebase";
 
 function Konten() {
+    const [error, setError] = useState("");
+    const { currentUser } = useAuth();
+    const navigate = useNavigate(); // Ganti useHistory dengan useNavigate
+  
+    useEffect(() => {
+      if (!currentUser) {
+        // Redirect or handle the case when the user is not logged in
+        navigate('/login');
+      }
+    }, [currentUser, navigate]);
+  
+    const handleLogout = async () => {
+      try {
+        console.log('Logging out...');
+        await auth.signOut();
+        console.log('Logout successful');
+        navigate('/login');
+      } catch (error) {
+        console.error('Error during sign-out:', error.message);
+      }
+    };
+    if (!currentUser) {
+      // You can also handle the case before useEffect runs
+      return null;
+    }
     return <div className="one-wrap">
         <div className="two-wrap">
         <div className="overlap">
@@ -24,36 +53,39 @@ function Konten() {
             <h3 className="judul">Account</h3>
             <div className="fill">
                 <img className="foto" src="./images/isi1.png"></img>
-                <p className="teks">Personal Information</p>
+                <p className="teks pt-2">Personal Information</p>
             </div>
             <div className="fill">
                 <img className="foto" src="./images/isi2.png"></img>
-                <p className="teks">Privacy & Security</p>
+                <p className="teks pt-2">Privacy & Security</p>
             </div>
             <div className="fill">
                 <img className="foto" src="./images/isi3.png"></img>
-                <p className="teks">Data Recapitulation</p>
+                <p className="teks pt-2">Data Recapitulation</p>
             </div>        
             </div>
         <div className="help">
             <h3 className="judul">Help Center</h3>
             <div className="fill">
                 <img className="foto" src="./images/isi4.png"></img>
-                <p className="teks">Settings</p>
+                <p className="teks pt-3">Settings</p>
             </div>
             <div className="fill">
                 <img className="foto" src="./images/isi5.png"></img>
-                <p className="teks">Help</p>
+                <p className="teks pt-3">Help</p>
             </div>
             <div className="fill">
                 <img className="foto" src="./images/isi6.png"></img>
-                <p className="teks">Report Problem / Bug</p>
+                <p className="teks pt-3">Report Problem / Bug</p>
             </div>
             <div className="fill">
                 <img className="foto1" src="./images/isi7.png"></img>
-                <p className="teks">About Us</p>
+                <p className="teks pt-3">About Us</p>
             </div>
-            <div className="out"></div>
+            <div className="fill">
+            <img className="foto1" src="./images/isi7.png"></img>
+                <button className="pt-3 pl-3" onClick={handleLogout}><span className="text-red-500">Sign out</span></button>
+            </div>
         </div>
         </div>
         </div>
